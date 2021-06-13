73aa4e62d97e56031d25b82f97618a2898155910
Vitalii Skubii <svorder@ukr.net>
2021-06-13 23:36:43 +0300

    full

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const controllers = require("../../../controllers/users.js");
 const guard = require("../../../helpers/guard");
 const validation = require("./validation");
+const upload = require("../../../helpers/upload");
 router.post("/registration", validation.validateRegister, controllers.reg);
 router.post("/login", validation.validateLogin, controllers.login);
 router.post("/logout", guard, controllers.logout);
 router.post("/current", controllers.getCurrentUser);
+router.patch("/avatars", [guard, upload.single("avatar")], controllers.avatars);
 
 module.exports = router;
